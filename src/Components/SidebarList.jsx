5c88cafce024680c83064d91b7ad9bb75045f6f8
Your Name <you@example.com>
2022-06-16 11:10:57 +0530

    modified component for functionality - sidebarlist

--- src/Components/SidebarList.jsx
+++ src/Components/SidebarList.jsx
@@ -2,8 +2,8 @@ import s from '../componentCSS/sidebar.module.css'
 
 function SidebarList (props) {
     return (
-        <li className={s.list}>
-        <button class="nav-link text-black" onClick={() => props.click(props.name)}>
+        <li className={s.list} onClick={() => props.click(props.name)}>
+        <button class="nav-link text-black" >
           {props.name}
         </button>
       </li>
